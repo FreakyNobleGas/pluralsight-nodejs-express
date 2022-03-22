ed948181a891edf13d072856af18d8526ee50b5d
Nick Quinn <31631401+FreakyNobleGas@users.noreply.github.com>
2022-03-22 16:04:09 -0400

    Instrumented routing for multiple pages

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// Set up Express
+// Set up Express. Everything works through app in express
 const express = require('express')
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// Setup debugging tool
+// Setup debugging tools
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan')
@@ -12,7 +12,7 @@ const morgan = require('morgan')
 app.use(morgan('tiny'))
 
 // Serve static HTML files in the public function. This function call explicitly looks for
-// a file called 'index.html'.
+// a file called 'index.html'. (Which is intentionally missing so we can call ejs files below)
 const path = require('path');
 app.use(express.static(path.join(__dirname, '/public/')));
 
@@ -20,12 +20,15 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-// Everything works through app in express
-// Setup routing
+// Base URL for app, this uses index.ejs since we are using res.render()
 app.get('/', (req, res)=>{
     // Pass variable to ejs and render website
     res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
 });
+
+// Sessions Router is defined in ./src/routers/sessionsRouter.js
+const sessionsRouter = require('./src/routers/sessionsRouter')
+app.use('/sessions', sessionsRouter)
 
 // Bind app to port
 app.listen(PORT, ()=> {
