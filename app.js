ecd78b99998e0d999e6cbc4bb23b6a23dc36989c
Nick Quinn <31631401+FreakyNobleGas@users.noreply.github.com>
2022-03-22 13:47:22 -0400

    Added first html page using express and installed debugging tools.

---
+++ app.js
@@ -0,0 +1,28 @@
+// Set up Express
+const express = require('express')
+const app = express();
+
+// Setup debugging tool
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan')
+
+// Setup morgan to log web traffic
+app.use(morgan('tiny'))
+
+// Serve static HTML files in the public function. This function call explicitly looks for
+// a file called 'index.html'.
+const path = require('path'); 
+app.use(express.static(path.join(__dirname, '/public/')));
+
+// Everything works through app in express
+// Setup routing
+app.get('/', (req, res)=>{
+    res.send('Hello from my app');
+})
+
+// Bind app to port
+app.listen(3000, ()=> {
+    // Template String. Debug is used in place of console.log, which only runs in debug mode!
+    debug(`listening on port ${chalk.green('3000')}`);
+})
